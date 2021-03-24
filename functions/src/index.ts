ec88a898c14869ba4d35f4b94a49ee674e9f9af7
Luka-dot <79lukask@gmail.com>
2021-03-24 17:22:03 -0400

    express set up and add entries rout set up

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,17 @@
 import * as functions from "firebase-functions";
 import * as express from 'express';
 
+import { addEntry } from './entryController';
+
 const app = express();
 
 app.get('/', (req, res) => res.status(200).send('Hello for Lukas app.express.'))
+app.post('/entries', addEntry);
 
 exports.app = functions.https.onRequest(app);
+
+
+
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
