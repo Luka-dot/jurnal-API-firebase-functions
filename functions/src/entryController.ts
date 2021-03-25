bbb13bad6a3aa11333dfe8ae255ecf3dad6316e9
Luka-dot <79lukask@gmail.com>
2021-03-24 22:10:59 -0400

    update and delete set up

--- functions/src/entryController.ts
+++ functions/src/entryController.ts
@@ -37,12 +37,56 @@ const addEntry = async (req: Request, res: Response) => {
 
 const getAllEntries = async (req: Request, res: Response) => {
     try {
-      const allEntries = await db.collection('entries').get();
+        const allEntries: EntryType[] = [];
+        const querySnapshot = await db.collection('entries').get();
+        querySnapshot.forEach((doc: any) =>  allEntries.push(doc.data()))
 
-      return res.status(200).json(allEntries.docs);
+        return res.status(200).json(allEntries);
     } catch (error) {
        return res.status(500).json(error.message);
     }
 };
 
-export { addEntry, getAllEntries };
+const updateEntry = async (req: Request, res: Response) => {
+    const { body: { text, title }, params: { entryId} } = req;
+
+    try {
+        const entry = db.collection('entries').doc(entryId);
+        const currentData = (await entry.get()).data() || {};
+
+        const entryObject = {
+            title: title || currentData.title,
+            text: text || currentData.text
+        }
+
+        await entry.set(entryObject)
+
+        return res.status(200).json({
+            status: 'success',
+            message: 'update entry was successfull',
+            data: entryObject
+        })
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
+}
+
+const deleteEntry = async (req: Request, res: Response) => {
+    const { entryId } = req.params;
+
+    try {
+        const entry = await db.collection('entries').doc(entryId);
+
+        await entry.delete();
+
+        return res.status(200).json({
+            status: 'success',
+            message: 'delete entry was successfull',
+        })
+        
+    } catch (error) {
+        return res.status(500).json(error.message);
+    }
+}
+
+export { addEntry, getAllEntries, updateEntry, deleteEntry };
